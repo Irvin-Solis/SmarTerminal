bdbeaaab3986d86e891124302fd6fe984637bad9
Jimenez-Solis, Irvin <irvin.jimenez-solis2@cerner.com>
2024-09-29 01:33:53 -0500

    terminal-ui

--- react-ts/src/renderer/src/App.tsx
+++ react-ts/src/renderer/src/App.tsx
@@ -1,67 +1,37 @@
 import Versions from './components/Versions';
-import electronLogo from './assets/electron.svg';
-import { useXTerm } from 'react-xtermjs';
-import { useMemo, useRef } from 'react';
-
-const { os } = window.api;
+import Terminal, { ColorMode, TerminalOutput } from 'react-terminal-ui';
+import { useState, } from 'react';
 
 function App(): JSX.Element {
-  const ipcHandle = (): void => window.electron.ipcRenderer.send('ping');
-  const { ref, instance } = useXTerm();
 
-  useMemo(() => {
-    instance?.onData((data) => {
-      instance.write(data);
-    });
-  }, []);
+  const [terminalLineData, setTerminalLineData] = useState();
+  const [terminalPromp, setTerminalPromp] = useState('bash$');
 
-  // useMemo(() => {
-  //   instance.onResize((size) => {});
-  // }, []);
+  const HandleInput = (terminalInput) => {
+    if ('/'=== terminalInput){
 
-  // const handleData = useCallback((e) => {
-  //   console.log(e);
-  // }, []);
+    }
+  }
 
-  // const handleResize = useCallback((e) => {
-  //   console.log(e);
-  // }, []);
-
+  const TerminalController = (props = {}) => {
+    return (
+        <Terminal prompt={terminalPromp} height='20vw' colorMode={ ColorMode.Dark }  onInput={ terminalInput => {HandleInput(terminalInput)} }>
+         <TerminalOutput>{terminalLineData}</TerminalOutput>
+        </Terminal>
+    )
+  };
   return (
     <>
-      <img alt="logo" className="logo" src={electronLogo} />
-      <div className="creator">Powered by electron-vite</div>
-      <div className="text">
-        Build an Electron app with <span className="react">React</span>
-        &nbsp;and <span className="ts">TypeScript</span>
-      </div>
-      <p className="tip">
-        Please try pressing <code>F12</code> to open the devTool
-      </p>
-      <div className="actions">
-        <div className="action">
-          <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">
-            Documentation
-          </a>
-        </div>
-        <div className="action">
-          <a target="_blank" rel="noreferrer" onClick={ipcHandle}>
-            Send IPC
-          </a>
-        </div>
-      </div>
       <Versions></Versions>
-
-      <div ref={ref} />
-
-      {/* <XTerm
-        options={{ cursorBlink: true }}
-        style={{ width: '100%', height: '100%' }}
-        listeners={{
-          onData: handleData,
-          onResize: handleResize
-        }}
-      /> */}
+      <div className="container" style={{ width: '80vw', display: 'flex' }}>
+        <div><TerminalController /></div>
+        <div style={{
+          width: '20%',
+          height: 'auto',
+          backgroundColor: 'grey',
+          border: '1px solid black'
+        }}></div>
+      </div>
     </>
   );
 }
